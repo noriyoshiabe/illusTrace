8c1d7d5fda75b82e12f21fb8325c33fcab75d50c
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2016-06-11 08:11:13 +0900

    getopt

--- cli/cli.cpp
+++ cli/cli.cpp
@@ -1,12 +1,65 @@
 #include "CLI.h"
 
 #include <iostream>
+#include <getopt.h>
 
 using namespace illustrace;
 using namespace cli;
 
+const std::string CLI::VERSION = "0.1.0";
+
 int CLI::run(int argc, char **argv)
 {
+    static struct option _options[] = {
+        { "help", no_argument, NULL, 'h'},
+        { "version", no_argument, NULL, 'v'},
+
+        {NULL, 0, NULL, 0}
+    };
+
+    int opt;
+    const char *input = NULL;
+
+    CLI cli;
+
+    while (-1 != (opt = getopt_long(argc, argv, "hv", _options, NULL))) {
+        switch (opt) {
+        case 'h':
+            cli.help();
+            return EXIT_SUCCESS;
+        case 'v':
+            cli.version();
+            return EXIT_SUCCESS;
+        case '?':
+            return EXIT_FAILURE;
+        }
+    }
+    
     std::cout << "Hi!!" << std::endl;
     return 0;
+}
+
+void CLI::help()
+{
+    const std::string HEADER =
+        "Welcome to the illustrace.\n"
+        "\n"
+        "illustrace CLI (beta): version " + VERSION + "\n"
+        "Copyright (c) 2016, Noriyoshi Abe. All Rights Reserved.\n";
+
+    const std::string USAGE =
+        "Usage: illustrace [options] [file]\n"
+        "Options:\n"
+        "  -h, --help    This help text.\n"
+        "  -v, --version Show program version.\n";
+    
+    std::cout << HEADER;
+    std::cout << std::endl;
+    std::cout << USAGE;
+    std::cout << std::endl;
+}
+
+void CLI::version()
+{
+    std::cout << "illustrace CLI Version " << VERSION << " June 11, 2016" << std::endl;
 }
--- cli/cli.h
+++ cli/cli.h
@@ -1,11 +1,17 @@
 #pragma once
 
+#include <string>
+
 namespace illustrace {
 namespace cli {
 
 class CLI {
 public:
     static int run(int argc, char **argv);
+    static void help();
+    static void version();
+
+    static const std::string VERSION; 
 };
 
 } // namespace cli
