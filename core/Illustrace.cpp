bb71cbd9520c083fd44af7bfb52fd4ab1e636d4f
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2016-06-11 10:15:17 +0900

    read image file with opencv

--- core/Illustrace.cpp
+++ core/Illustrace.cpp
@@ -1,12 +1,25 @@
 #include "Illustrace.h"
 
 #include <iostream>
+#include "opencv2/highgui.hpp"
 
 using namespace illustrace;
 using namespace core;
 
 bool Illustrace::loadSourceImage(const char *filename)
 {
-    std::cout << filename << std::endl;
+    sourceImage = imread(filename, cv::IMREAD_GRAYSCALE);
+    if (!sourceImage.data) {
+        return false;
+    }
+    
+    setResultImage(sourceImage);
     return true;
+}
+
+void Illustrace::setResultImage(cv::Mat &image)
+{
+    resultImage = image;
+    
+    // TODO notify
 }
--- core/Illustrace.h
+++ core/Illustrace.h
@@ -1,11 +1,19 @@
 #pragma once
 
+#include "opencv2/imgproc.hpp"
+
 namespace illustrace {
 namespace core {
 
 class Illustrace {
 public:
     bool loadSourceImage(const char *filename);
+
+private:
+    cv::Mat sourceImage;
+    cv::Mat resultImage;
+
+    void setResultImage(cv::Mat &image);
 };
 
 } // namespace core
