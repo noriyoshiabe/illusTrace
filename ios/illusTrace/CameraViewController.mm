8a1a7c9dcd6ac59ef1164c52f41692bd11a67052
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2016-07-01 02:59:04 +0900

    Preview outline in realtime :grinning:

--- ios/illusTrace/CameraViewController.mm
+++ ios/illusTrace/CameraViewController.mm
@@ -19,6 +19,8 @@ using namespace illustrace;
     CvVideoCamera* _videoCamera;
     Document *_document;
     Illustrace _illustrace;
+    CGColorSpaceRef _colorSpace;
+    CGContextRef _bitmapContext;
 }
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @end
@@ -38,6 +40,8 @@ using namespace illustrace;
     _videoCamera.defaultAVCaptureVideoOrientation = AVCaptureVideoOrientationPortrait;
     _videoCamera.defaultFPS = 30;
     _videoCamera.grayscaleMode = YES;
+    
+    _colorSpace = CGColorSpaceCreateDeviceGray();
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -73,11 +77,59 @@ using namespace illustrace;
     // Dispose of any resources that can be recreated.
 }
 
+- (void)drawPaths
+{
+    CGContextSetStrokeColorWithColor(_bitmapContext, [UIColor blackColor].CGColor);
+    CGContextSetLineWidth(_bitmapContext, _document->thickness());
+    CGContextSetLineCap(_bitmapContext, kCGLineCapRound);
+    
+    for (auto *path : *_document->paths()) {
+        CGContextBeginPath(_bitmapContext);
+        
+        [self drawPath:path];
+        
+        if (path->closed) {
+            CGContextEOFillPath(_bitmapContext);
+        }
+        
+        CGContextStrokePath(_bitmapContext);
+    }
+}
+
+- (void)drawPath:(Path *)path
+{
+    for (Segment &s : path->segments) {
+        switch (s.type) {
+            case Segment::Type::Move:
+                CGContextMoveToPoint(_bitmapContext, s[2].x, s[2].y);
+                break;
+            case Segment::Type::Line:
+                CGContextAddLineToPoint(_bitmapContext, s[2].x, s[2].y);
+                break;
+            case Segment::Type::Curve:
+                CGContextAddCurveToPoint(_bitmapContext, s[0].x, s[0].y, s[1].x, s[1].y, s[2].x, s[2].y);
+                break;
+        }
+    }
+    
+    for (Path *child : path->children) {
+        [self drawPath:child];
+    }
+}
+
 #pragma mark - CvVideoCameraDelegate
 
 - (void)processImage:(cv::Mat &)image
 {
     _illustrace.traceForPreview(image, _document);
+    
+    if (!_bitmapContext) {
+        _bitmapContext = CGBitmapContextCreate(image.data, image.cols, image.rows, 8, image.cols, _colorSpace, kCGImageAlphaNone);
+        CGAffineTransform flipVertical = CGAffineTransformMake(1, 0, 0, -1, 0, image.rows);
+        CGContextConcatCTM(_bitmapContext, flipVertical);
+    }
+    
+    [self drawPaths];
 }
 
 @end
