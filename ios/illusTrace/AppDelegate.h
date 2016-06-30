96d90f3eff15bb945c809c4f7ca046af2ffdccf0
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2016-06-30 22:07:08 +0900

    iOS project

---
+++ ios/illusTrace/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  illusTrace
+//
+//  Created by abechan on 2016/06/30.
+//  Copyright © 2016年 Noriyoshi Abe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios/illusTrace/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  illusTrace
+//
+//  Created by abechan on 2016/06/30.
+//  Copyright © 2016年 Noriyoshi Abe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
