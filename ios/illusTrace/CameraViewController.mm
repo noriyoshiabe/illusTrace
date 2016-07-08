f29f7f20ea720bd0cbcd5af2979ba9fa34df3073
noriyoshiabe <noriyoshi.abe.0212@gmail.com>
2016-07-09 02:04:18 +0900

    Camera preview and still image

--- ios/illusTrace/CameraViewController.mm
+++ ios/illusTrace/CameraViewController.mm
@@ -7,30 +7,18 @@
 //
 
 #import "CameraViewController.h"
-#import <opencv2/videoio/cap_ios.h>
 #import "Illustrace.h"
+#import <AVFoundation/AVFoundation.h>
 
 using namespace illustrace;
 
-@interface CameraViewController () <CvVideoCameraDelegate> {
-    CvVideoCamera* _videoCamera;
-    Document *_document;
-    Illustrace _illustrace;
-    CGColorSpaceRef _colorSpace;
-    CGContextRef _bitmapContext;
-    uchar *_imageData;
-    
-    AVCaptureDevice *_videoDevice;
-    CGSize _dimensions;
-    CGFloat _zoomScale;
+@interface CameraViewController () <AVCaptureVideoDataOutputSampleBufferDelegate> {
+    AVCaptureDeviceInput *_videoInput;
+    AVCaptureStillImageOutput *_stillImageOutput;
+    AVCaptureVideoDataOutput *_videoDataOutput;
+    AVCaptureSession *_session;
 }
-@property (weak, nonatomic) IBOutlet UIImageView *imageView;
-@property (weak, nonatomic) IBOutlet UISlider *brightnessSlider;
-@property (weak, nonatomic) IBOutlet UISlider *detailSlider;
-@property (weak, nonatomic) IBOutlet UISlider *smoothingSlider;
-@property (weak, nonatomic) IBOutlet UISlider *thicknessSlider;
-@property (weak, nonatomic) IBOutlet UISegmentedControl *colorControl;
-@property (weak, nonatomic) IBOutlet UISwitch *lightSwitch;
+@property (weak, nonatomic) IBOutlet UIImageView *previewView;
 @end
 
 @implementation CameraViewController
@@ -39,84 +27,38 @@ using namespace illustrace;
 {
     [super viewDidLoad];
     
-    _document = new Document();
+    NSError *error = nil;
+    _session = [[AVCaptureSession alloc] init];
+    _session.sessionPreset = AVCaptureSessionPresetPhoto;
     
-    _videoCamera = [[CvVideoCamera alloc] initWithParentView:_imageView];
-    _videoCamera.delegate = self;
-    _videoCamera.defaultAVCaptureDevicePosition = AVCaptureDevicePositionBack;
-    _videoCamera.defaultAVCaptureSessionPreset = AVCaptureSessionPresetPhoto;
-    _videoCamera.defaultAVCaptureVideoOrientation = AVCaptureVideoOrientationPortrait;
-    _videoCamera.defaultFPS = 30;
+    AVCaptureDevice *camera = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     
-    _colorSpace = CGColorSpaceCreateDeviceRGB();
+    _videoInput = [[AVCaptureDeviceInput alloc] initWithDevice:camera error:&error];
+    [_session addInput:_videoInput];
     
-    _imageView.userInteractionEnabled = YES;
+    _videoDataOutput = [[AVCaptureVideoDataOutput alloc] init];
+    [_session addOutput:_videoDataOutput];
     
-    UIPinchGestureRecognizer *pinchGestureRecognizer = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(pinchGestureRecognizerAction:)];
-    [_imageView addGestureRecognizer:pinchGestureRecognizer];
+    dispatch_queue_t queue = dispatch_queue_create("captureQueue", NULL);
+    _videoDataOutput.alwaysDiscardsLateVideoFrames = YES;
+    _videoDataOutput.videoSettings = @{(NSString *)kCVPixelBufferPixelFormatTypeKey: @(kCVPixelFormatType_32BGRA)};
+    [_videoDataOutput setSampleBufferDelegate:self queue:queue];
     
-    UITapGestureRecognizer *singleTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(singleTapGestureRecognizerAction:)];
-    singleTapRecognizer.numberOfTapsRequired = 1;
-    [_imageView addGestureRecognizer:singleTapRecognizer];
-    UITapGestureRecognizer *doubleTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(doubleTapGestureRecognizerAction:)];
-    doubleTapRecognizer.numberOfTapsRequired = 2;
-    [_imageView addGestureRecognizer:doubleTapRecognizer];
-    [singleTapRecognizer requireGestureRecognizerToFail:doubleTapRecognizer];
-    
-    NSArray *devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
-    for (AVCaptureDevice *device in devices) {
-        if (AVCaptureDevicePositionBack == [device position]) {
-            _videoDevice = device;
-            
-            if ([_videoDevice lockForConfiguration:nil]) {
-                _videoDevice.focusMode = AVCaptureFocusModeAutoFocus;
-                _videoDevice.exposureMode = AVCaptureExposureModeAutoExpose;
-                [_videoDevice unlockForConfiguration];
-            }
-            
-            _lightSwitch.enabled = _videoDevice.hasTorch;
-        }
-    }
+    _stillImageOutput = [[AVCaptureStillImageOutput alloc] init];
+    _stillImageOutput.outputSettings = @{(NSString *)kCVPixelBufferPixelFormatTypeKey: @(kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange)};
+    [_session addOutput:_stillImageOutput];
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-    
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [_videoCamera start];
-        
-        dispatch_async(dispatch_get_main_queue(), ^{
-            AVCaptureInput *input = [_videoCamera.captureSession.inputs objectAtIndex:0];
-            AVCaptureInputPort *port = [input.ports objectAtIndex:0];
-            CMFormatDescriptionRef formatDescription = port.formatDescription;
-            CMVideoDimensions dimentions = CMVideoFormatDescriptionGetDimensions(formatDescription);
-            _dimensions.width = dimentions.width < dimentions.height ? dimentions.width : dimentions.height;
-            _dimensions.height = dimentions.width< dimentions.height ? dimentions.height : dimentions.width;
-            
-#if 0
-            NSLog( @"dimensions: %f %f", _dimensions.width, _dimensions.height);
-#endif
-            
-            CALayer *layer = [_imageView.layer.sublayers objectAtIndex:0];
-            
-            CGFloat ratiox = _dimensions.width / layer.frame.size.width;
-            CGFloat ratioy = _dimensions.height / layer.frame.size.height;
-            CGFloat ratio = (ratiox < ratioy)? ratiox : ratioy;
-            CGFloat w = _dimensions.width / ratio;
-            CGFloat h = _dimensions.height / ratio;
-            CGFloat x = (layer.frame.size.width - w) / 2.0;
-            CGFloat y = (layer.frame.size.height - h) / 2.0;
-            
-            layer.frame = CGRectMake(x, y, w, h);
-        });
-    });
+    [_session startRunning];
 }
 
 - (void)viewDidDisappear:(BOOL)animated
 {
-    [super viewWillDisappear:true];
-    [_videoCamera stop];
+    [super viewWillDisappear:animated];
+    [_session stopRunning];
 }
 
 - (void)didReceiveMemoryWarning
@@ -125,154 +67,53 @@ using namespace illustrace;
     // Dispose of any resources that can be recreated.
 }
 
-- (void)drawPaths
+- (IBAction)takeButtonAction:(id)sender
 {
-    if (1 == _colorControl.selectedSegmentIndex) {
-        CGContextSetRGBFillColor(_bitmapContext, 1.0, 1.0, 1.0, 1.0);
-        CGContextFillRect(_bitmapContext, (CGRect){CGPointZero, {_dimensions.width, _dimensions.height}});
-        
-        CGContextSetRGBStrokeColor(_bitmapContext, 0.0, 0.0, 0.0, 1.0);
-        CGContextSetRGBFillColor(_bitmapContext, 0.0, 0.0, 0.0, 1.0);
+    AVCaptureConnection *videoConnection = [_stillImageOutput connectionWithMediaType:AVMediaTypeVideo];
+    if (videoConnection) {
+        [_stillImageOutput captureStillImageAsynchronouslyFromConnection:videoConnection completionHandler:^(CMSampleBufferRef imageDataSampleBuffer, NSError *error) {
+             if (imageDataSampleBuffer) {
+                 CVImageBufferRef pixelBuffer = CMSampleBufferGetImageBuffer(imageDataSampleBuffer);
+                 CVPixelBufferLockBaseAddress(pixelBuffer, 0);
+                 uint8_t *grayImage = (uint8_t *)CVPixelBufferGetBaseAddress(pixelBuffer);
+                 
+                 NSLog(@"#### %lu %lu", CVPixelBufferGetWidth(pixelBuffer), CVPixelBufferGetHeight(pixelBuffer));
+                 
+                 CVPixelBufferUnlockBaseAddress(pixelBuffer, 0);
+             }
+         }];
     }
-    else {
-        CGContextSetRGBStrokeColor(_bitmapContext, 0.0, 1.0, 0.0, 0.66);
-        CGContextSetRGBFillColor(_bitmapContext, 0.0, 1.0, 0.0, 0.33);
-    }
+}
+
+#pragma mark AVCaptureVideoDataOutputSampleBufferDelegate
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection
+{
+    CVImageBufferRef pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+    CVPixelBufferLockBaseAddress(pixelBuffer, 0);
+    uint8_t *imageAdress = (uint8_t *)CVPixelBufferGetBaseAddress(pixelBuffer);
     
-    CGContextSetLineWidth(_bitmapContext, _document->thickness());
-    CGContextSetLineCap(_bitmapContext, kCGLineCapRound);
+    // TODO scan
     
-    for (auto *path : *_document->paths()) {
-        CGMutablePathRef pathRef = CGPathCreateMutable();
-        
-        [self drawPath:path subPath:pathRef];
-        
-        if (path->closed) {
-            CGContextAddPath(_bitmapContext, pathRef);
-            CGContextEOFillPath(_bitmapContext);
-        }
-        
-        CGContextAddPath(_bitmapContext, pathRef);
-        CGContextStrokePath(_bitmapContext);
-        
-        CGPathRelease(pathRef);
-    }
-}
-
-- (void)drawPath:(Path *)path subPath:(CGMutablePathRef)subPath
-{
-    for (Segment &s : path->segments) {
-        switch (s.type) {
-            case Segment::Type::Move:
-                CGPathMoveToPoint(subPath, NULL, s[2].x, s[2].y);
-                break;
-            case Segment::Type::Line:
-                CGPathAddLineToPoint(subPath, NULL, s[2].x, s[2].y);
-                break;
-            case Segment::Type::Curve:
-                CGPathAddCurveToPoint(subPath, NULL, s[0].x, s[0].y, s[1].x, s[1].y, s[2].x, s[2].y);
-                break;
-        }
-    }
+    uint8_t *baseAddress = (uint8_t *)CVPixelBufferGetBaseAddressOfPlane(pixelBuffer, 0);
+    size_t bytesPerRow = CVPixelBufferGetBytesPerRow(pixelBuffer);
+    size_t width = CVPixelBufferGetWidth(pixelBuffer);
+    size_t height = CVPixelBufferGetHeight(pixelBuffer);
     
-    for (Path *child : path->children) {
-        [self drawPath:child subPath:subPath];
-    }
-}
-
-#pragma mark - CvVideoCameraDelegate
-
-- (void)processImage:(cv::Mat &)image
-{
-    _illustrace.traceForPreview(image, _document);
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef bitmapContext = CGBitmapContextCreate(baseAddress, width, height, 8, bytesPerRow, colorSpace, kCGBitmapByteOrder32Little | kCGImageAlphaPremultipliedFirst);
     
-    if (_imageData != image.data) {
-        if (_bitmapContext) {
-            CGContextRelease(_bitmapContext);
-            _bitmapContext = NULL;
-        }
-        
-        _imageData = image.data;
-    }
+    CGImageRef cgImage = CGBitmapContextCreateImage(bitmapContext);
+    UIImage *image = [UIImage imageWithCGImage:cgImage scale:1.0 orientation:UIImageOrientationRight];
     
-    if (!_bitmapContext) {
-        _bitmapContext = CGBitmapContextCreate(image.data, image.cols, image.rows, 8, image.cols * 4, _colorSpace, kCGImageAlphaPremultipliedLast);
-        CGAffineTransform flipVertical = CGAffineTransformMake(1, 0, 0, -1, 0, image.rows);
-        CGContextConcatCTM(_bitmapContext, flipVertical);
-    }
+    CGContextRelease(bitmapContext);
+    CGColorSpaceRelease(colorSpace);
+    CVPixelBufferUnlockBaseAddress(pixelBuffer, 0);
+    CGImageRelease(cgImage);
     
-    [self drawPaths];
-}
-
-#pragma mark IBAction
-
-- (IBAction)brightnessSliderAction:(UISlider *)sender
-{
-    _document->brightness(sender.value);
-}
-
-- (IBAction)detailSliderAction:(UISlider *)sender
-{
-    _document->detail(MAX(0.1, sender.value));
-}
-
-- (IBAction)smoothingSliderAction:(UISlider *)sender
-{
-    _document->smoothing(sender.value);
-}
-
-- (IBAction)thicknessSliderAction:(UISlider *)sender
-{
-    _document->thickness(sender.value);
-}
-
-- (IBAction)lightSwitchAction:(UISwitch *)sender
-{
-    if ([_videoDevice lockForConfiguration:nil]) {
-        [_videoDevice setTorchMode:sender.isOn ? AVCaptureTorchModeOn : AVCaptureTorchModeOff];
-        _videoDevice.focusMode = AVCaptureFocusModeAutoFocus;
-        _videoDevice.exposureMode = AVCaptureExposureModeAutoExpose;
-        [_videoDevice unlockForConfiguration];
-    }
-}
-
-#pragma mark GestureRecognizer
-
-- (void)pinchGestureRecognizerAction:(UIPinchGestureRecognizer *)sender
-{
-    if (UIGestureRecognizerStateBegan == sender.state) {
-        _zoomScale = _videoDevice.videoZoomFactor;
-    }
-    
-    if (UIGestureRecognizerStateChanged == sender.state) {
-        if ([_videoDevice lockForConfiguration:nil]) {
-            _videoDevice.videoZoomFactor = MAX(1.0, MIN(_zoomScale * sender.scale, _videoDevice.activeFormat.videoMaxZoomFactor));
-            [_videoDevice unlockForConfiguration];
-        }
-    }
-}
-
-- (void)singleTapGestureRecognizerAction:(UITapGestureRecognizer *)sender
-{
-    if ([_videoDevice lockForConfiguration:nil]) {
-        CGPoint point = [sender locationInView: _imageView];
-        point.x /= _imageView.frame.size.width;
-        point.y /= _imageView.frame.size.height;
-        
-        _videoDevice.focusPointOfInterest = point;
-        _videoDevice.focusMode = AVCaptureFocusModeAutoFocus;
-        _videoDevice.exposurePointOfInterest = point;
-        _videoDevice.exposureMode = AVCaptureExposureModeAutoExpose;
-        [_videoDevice unlockForConfiguration];
-    }
-}
-
-- (void)doubleTapGestureRecognizerAction:(UITapGestureRecognizer *)sender
-{
-    if ([_videoDevice lockForConfiguration:nil]) {
-        _videoDevice.videoZoomFactor = 1.0;
-        [_videoDevice unlockForConfiguration];
-    }
+    dispatch_async(dispatch_get_main_queue(), ^{
+        _previewView.image = image;
+    });
 }
 
 @end
